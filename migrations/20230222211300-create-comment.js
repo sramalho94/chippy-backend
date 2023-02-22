2211e491c9dc9697580b8d7f807ead021570965f
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-02-22 16:16:35 -0500

    Set up initial comments migration

---
+++ migrations/20230222211300-create-comment.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('comments', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'users',
+					key: 'id'
+				}
+			},
+			chipId: {
+				type: Sequelize.INTEGER,
+				onDelete: 'CASCADE',
+				references: {
+					model: 'chips',
+					key: 'id'
+				}
+			},
+			comment: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('comments');
+	}
+};
