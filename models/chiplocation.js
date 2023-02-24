b7937154569a66a42aba76ee76151a0d84a4e293
jeremyHarrell <hillvulf@gmail.com>
2023-02-24 11:20:57 -0600

    ChipLocation associations complete

--- models/chiplocation.js
+++ models/chiplocation.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-				
+				ChipLocation.belongsTo(models.User, {
+					foreignKey: 'userId'
+				})
 		}
 	}
 	ChipLocation.init(
