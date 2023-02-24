f68c5f051be752b4f82b90650af27b1d20a551e7
jeremyHarrell <hillvulf@gmail.com>
2023-02-24 11:18:59 -0600

    Achievement Associations complete

--- models/achievement.js
+++ models/achievement.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			Achievement.belongsToMany(models.User, {
+				through: models.UserAchievements,
+				as: 'achievement',
+				foreignKey: 'achievementId'
+			})
 		}
 	}
 	Achievement.init(
