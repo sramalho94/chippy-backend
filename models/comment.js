98dc2ae5d659aa45bb7c970de688158f70da5e10
jeremyHarrell <hillvulf@gmail.com>
2023-02-24 11:16:29 -0600

    Comment Associations complete

--- models/comment.js
+++ models/comment.js
@@ -8,7 +8,16 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			Comment.belongsToMany(models.Comment, {
+				through: models.CommentRemark,
+				as: 'comment',
+				foreignKey: 'commentId'
+			})
+			Comment.belongsToMany(models.Comment, {
+				through: models.CommentRemark,
+				as: 'remark',
+				foreignKey: 'remarkId'
+			})
 		}
 	}
 	Comment.init(
