abac3f549685519bc90cb3bb864f2fc208ca100d
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-02-25 11:25:35 -0500

    Added user controllers

---
+++ controllers/UserController.js
@@ -0,0 +1,39 @@
+const { User } = require('../models');
+
+const GetUserById = async (req, res) => {
+	try {
+		const user = await User.findOne({ where: { id: req.params.userId } });
+		res.send(user);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+const UpdateUser = async (req, res) => {
+	try {
+		const userId = parseInt(req.params.userId);
+		const updatedUser = await User.update(req.body, {
+			where: { id: userId },
+			returning: true
+		});
+		res.send(updatedUser);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+const DeleteUser = async (req, res) => {
+	try {
+		let userId = req.params.userId;
+		await User.destroy({ where: { id: userId } });
+		res.send({ message: `Deleted user with an id of ${userId}` });
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+module.exports = {
+	GetUserById,
+	UpdateUser,
+	DeleteUser
+};
