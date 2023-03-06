53ece92b618f72db0ead62981d3086efc82a77f3
jeremyHarrell <hillvulf@gmail.com>
2023-03-06 12:25:40 -0600

    Get User Achievements By User Id router complete

--- routes/UserAchievementRouter.js
+++ routes/UserAchievementRouter.js
@@ -2,4 +2,9 @@ const router = require('express').Router()
 const controller = require('../controllers/UserAchievementController')
 const middleware = require('../middleware')
 
+router.get(
+  '/:userId',
+  controller.GetUserAchievementsByUserId
+)
+
 module.exports = router
