731c925163cb507f1ae94c564533430f2fac74d5
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-03-07 11:55:09 -0500

    Added LocationRouter

---
+++ routes/LocationRouter.js
@@ -0,0 +1,23 @@
+const Router = require('express').Router();
+const controller = require('../controllers/LocationController');
+const middleware = require('../middleware');
+
+Router.get('/:locationId', controller.GetLocationsById);
+Router.post(
+	'/post',
+	middleware.stripToken,
+	middleware.verifyToken,
+	controller.CreateLocation
+);
+Router.post(
+	'/update',
+	middleware.stripToken,
+	middleware.verifyToken,
+	controller.UpdateLocation
+);
+Router.delete(
+	'/delete',
+	middleware.stripToken,
+	middleware.verifyToken,
+	controller.DeleteLocation
+);
