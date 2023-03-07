f3e09b5c7e7258452f79a3f5e38d6a364777b5c1
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-03-07 11:38:43 -0500

    Added AuthRouter and changed controller files to singular

---
+++ controllers/UserAchievementController.js
@@ -0,0 +1,26 @@
+const { UserAchievements } = require('../models');
+
+const GetUserAchievementsByUserId = async (req, res) => {
+	try {
+		const userAchievements = await UserAchievements.findAll({
+			where: { userId: req.params.userId }
+		});
+		res.send(userAchievements);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+const CreateUserAchievement = async (req, res) => {
+	try {
+		const userAchievement = await UserAchievements.create({ ...req.body });
+		res.send(userAchievement);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
+
+module.exports = {
+	GetUserAchievementsByUserId,
+	CreateUserAchievement
+};
