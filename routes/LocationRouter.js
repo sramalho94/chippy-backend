716e5db9e07bdca7bae2a1644da91cb65b6751a6
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-17 17:30:03 -0400

    corrections to put and delete routes

--- routes/LocationRouter.js
+++ routes/LocationRouter.js
@@ -1,25 +1,25 @@
-const Router = require('express').Router();
-const controller = require('../controllers/LocationController');
-const middleware = require('../middleware');
+const Router = require('express').Router()
+const controller = require('../controllers/LocationController')
+const middleware = require('../middleware')
 
-Router.get('/:locationId', controller.GetLocationsById);
+Router.get('/:userId', controller.GetLocationsById)
 Router.post(
-	'/post',
-	middleware.stripToken,
-	middleware.verifyToken,
-	controller.CreateLocation
-);
+  '/post',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateLocation
+)
 Router.put(
-	'/update',
-	middleware.stripToken,
-	middleware.verifyToken,
-	controller.UpdateLocation
-);
+  '/update/:locationId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateLocation
+)
 Router.delete(
-	'/delete',
-	middleware.stripToken,
-	middleware.verifyToken,
-	controller.DeleteLocation
-);
+  '/delete/:locationId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteLocation
+)
 
-module.exports = Router;
+module.exports = Router
