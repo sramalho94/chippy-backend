78408857ba472c98adeab9663b73b81a55f336d5
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-03-22 17:28:34 -0400

    Tweaked GetAllChipCommentsAndRemarks controller

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,54 +1,59 @@
-const { Comment, Chip, CommentRemark } = require('../models')
+const { Comment, Chip, CommentRemark } = require('../models');
 
 const GetAllChipCommentsAndRemarks = async (req, res) => {
-  try {
-    const comments = await Comment.findAll({ 
-      where: { id: req.params.chipId },
-      include: Chip, CommentRemark
-    })
-    res.send(comments)
-  } catch (error) {
-    return res.send(500).json({ error: error.message})
-  }
-}
+	try {
+		const comments = await Comment.findAll({
+			where: { chipId: req.params.chipId },
+			include: [
+				{
+					model: Comment,
+					as: 'remark',
+					through: { attributes: [] }
+				}
+			]
+		});
+		res.send(comments);
+	} catch (error) {
+		return res.send(500).json({ error: error.message });
+	}
+};
 
 const PostComment = async (req, res) => {
-  try {
-    const { chipId, userId, comment } = req.body
-    const newComment = await Comment.create({
-      chipId,
-      userId,
-      comment
-    })
-     res.send(newComment)
-  } catch (error) {
-    return res.status(500).json({ error: error.message})
-  }
-}
+	try {
+		const { chipId, userId, comment } = req.body;
+		const newComment = await Comment.create({
+			chipId,
+			userId,
+			comment
+		});
+		res.send(newComment);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
 
-const UpdateComment = async (req,res) => {
-  try {
-    const commentId = parseInt(req.params.commentId)
-    const updatedComment = await Comment.update(req.body, {
-      where: { id: commentId},
-      returning: true
-    })
-    res.send(updatedComment)
-  } catch (error) {
-    return res.status(500).json({ error: error.message})
-  }
-}
-
+const UpdateComment = async (req, res) => {
+	try {
+		const commentId = parseInt(req.params.commentId);
+		const updatedComment = await Comment.update(req.body, {
+			where: { id: commentId },
+			returning: true
+		});
+		res.send(updatedComment);
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
 
 const DeleteCommentByCommentId = async (req, res) => {
-  try {
-    const commentId = parseInt(req.params.commentId)
-    await Comment.destroy({ where: {id: commentId}})
-    res.send({ message: `Deleted comment with an id of ${commentId}`})
-  } catch (error) {
-    return res.status(500).json({ error: error.message})
-  }
-}
+	try {
+		const commentId = parseInt(req.params.commentId);
+		await Comment.destroy({ where: { id: commentId } });
+		res.send({ message: `Deleted comment with an id of ${commentId}` });
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+};
 
 // const DeleteCommentByChipId = async (req, res) => {
 //   try {
@@ -61,10 +66,9 @@ const DeleteCommentByCommentId = async (req, res) => {
 // }
 
 module.exports = {
-// DeleteCommentByChipId,
-DeleteCommentByCommentId,
-UpdateComment,
-PostComment,
-GetAllChipCommentsAndRemarks
-
-}
+	// DeleteCommentByChipId,
+	DeleteCommentByCommentId,
+	UpdateComment,
+	PostComment,
+	GetAllChipCommentsAndRemarks
+};
