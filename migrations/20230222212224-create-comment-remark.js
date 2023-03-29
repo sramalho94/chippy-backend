29bf5327536dcbf2c1b5545101d9741244127468
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-29 17:42:11 -0400

    updates to comment-remark migration

--- migrations/20230222212224-create-comment-remark.js
+++ migrations/20230222212224-create-comment-remark.js
@@ -1,41 +1,45 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable('comment_remarks', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER
-			},
-			commentId: {
-				type: Sequelize.INTEGER,
-				onDelete: 'CASCADE',
-				references: {
-					model: 'comments',
-					key: 'id'
-				}
-			},
-			remarkId: {
-				type: Sequelize.INTEGER,
-				onDelete: 'CASCADE',
-				references: {
-					model: 'comments',
-					key: 'id'
-				}
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE
-			}
-		});
-	},
-	async down(queryInterface, Sequelize) {
-		await queryInterface.dropTable('comment_remarks');
-	}
-};
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comment_remarks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      commentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'comments',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      remarkId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'comments',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comment_remarks')
+  }
+}
