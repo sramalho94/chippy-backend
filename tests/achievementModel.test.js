426ffae9e579ca02ada3c0566b8644f71bd76416
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-07 11:44:31 -0400

    updates to tests

--- tests/achievementModel.test.js
+++ tests/achievementModel.test.js
@@ -1,32 +1,29 @@
-process.env.NODE_ENV = 'test';
-const { Achievement } = require('../models');
+process.env.NODE_ENV = 'test'
+const { Achievement } = require('../models')
 
 describe('Achievement model tests', () => {
-	let testAchievement;
-	afterAll(async () => {
-		try {
-			await Achievement.destroy({ truncate: { cascade: true } });
-		} catch (error) {
-			console.error('Error cleaning up test data:', error);
-		}
-	});
-	test('create a new achievement', async () => {
-		try {
-			testAchievement = await Achievement.create({
-				name: 'Test Name',
-				icon: 'Test Icon',
-				description: 'Test Description'
-			});
-			expect(response.statusCode).toBe(201);
-			expect(testAchievement).toHaveProperty('id');
-			expect(testAchievement).toHaveProperty('name', 'Test Name');
-			expect(testAchievement).toHaveProperty('icon', 'Test Icon');
-			expect(testAchievement).toHaveProperty(
-				'description',
-				'Test Description'
-			);
-		} catch (error) {
-			console.error('Error creating achievement:', error);
-		}
-	});
-});
+  let testAchievement
+  afterAll(async () => {
+    try {
+      await Achievement.destroy({ truncate: { cascade: true } })
+    } catch (error) {
+      console.error('Error cleaning up test data:', error)
+    }
+  })
+  test('create a new achievement', async () => {
+    try {
+      testAchievement = await Achievement.create({
+        name: 'Test Name',
+        icon: 'Test Icon',
+        description: 'Test Description'
+      })
+      expect(testAchievement).toBeDefined()
+      expect(testAchievement).toHaveProperty('id')
+      expect(testAchievement).toHaveProperty('name', 'Test Name')
+      expect(testAchievement).toHaveProperty('icon', 'Test Icon')
+      expect(testAchievement).toHaveProperty('description', 'Test Description')
+    } catch (error) {
+      console.error('Error creating achievement:', error)
+    }
+  })
+})
--- tests/userAchievementController.test.js
+++ tests/userAchievementController.test.js
@@ -1,84 +1,76 @@
-process.env.NODE_ENV = 'test';
-const request = require('supertest');
-const app = require('../server');
-const { createToken, hashPassword } = require('../middleware');
-const { UserAchievements, User, Achievement } = require('../models');
+process.env.NODE_ENV = 'test'
+const request = require('supertest')
+const app = require('../server')
+const { createToken, hashPassword } = require('../middleware')
+const { UserAchievements, User, Achievement } = require('../models')
 
-let testToken;
+let testToken
 
 describe('UserAchievement controller tests', () => {
-	let testUserAchievement;
-	let userAchievementId;
-	let testAchievement;
-	let testAchievementId;
-	let testUser;
-	let testUserId;
-	beforeAll(async () => {
-		//create a test user
-		testUser = await User.create({
-			username: 'testuser',
-			passwordDigest: await hashPassword('testpassword')
-		});
-		testUserId = testUser.id;
+  let testUserAchievement
+  let userAchievementId
+  let testAchievement
+  let testAchievementId
+  let testUser
+  let testUserId
+  beforeAll(async () => {
+    //create a test user
+    testUser = await User.create({
+      username: 'testuser',
+      passwordDigest: await hashPassword('testpassword')
+    })
+    testUserId = testUser.id
 
-		//Login test user and get token
-		testToken = createToken({
-			id: testUserId,
-			username: testUser.username,
-			passwordDigest: testUser.passwordDigest
-		});
+    //Login test user and get token
+    testToken = createToken({
+      id: testUserId,
+      username: testUser.username,
+      passwordDigest: testUser.passwordDigest
+    })
 
-		//create a test achievement
-		testAchievement = await Achievement.create({
-			name: 'testachievement',
-			icon: 'testIcon',
-			description: 'testDescription'
-		});
-		testAchievementId = testAchievement.id;
+    //create a test achievement
+    testAchievement = await Achievement.create({
+      name: 'testachievement',
+      icon: 'testIcon',
+      description: 'testDescription'
+    })
+    testAchievementId = testAchievement.id
+  })
 
-		testUserAchievement = await UserAchievements.create({
-			userId: testUserId,
-			achievementId: testAchievementId
-		});
-	});
+  test('create user achievement', async () => {
+    const response = await request(app)
+      .post('/api/user-achievements/')
+      .set('Authorization', `Bearer ${testToken}`)
+      .send({
+        userId: testUserId,
+        achievementId: testAchievementId
+      })
+    userAchievementId = response.body.id
 
-	test('create user achievement', async () => {
-		const response = await request(app)
-			.post('/api/user-achievements')
-			.set('Authorization', `Bearer ${testToken}`)
-			.send({
-				userId: testUserId,
-				achievementId: testAchievementId,
-				passwordDigest: testUser.passwordDigest
-			});
-		userAchievementId = response.body.id;
+    expect(response.statusCode).toBe(201)
+    expect(response.body.userId).toBe(testUserId)
+    expect(response.body.achievementId).toBe(testAchievementId)
+  })
 
-		expect(response.statusCode).toBe(201);
-		expect(response.body.userId).toBe(testUserId);
-		expect(response.body.achievementId).toBe(testAchievementId);
-	});
-
-	test('get user achievements by user id', async () => {
-		const response = await request(app).get(
-			`/api/user-achievements/${testUserId}`
-		);
-		console.log(response);
-		expect(response.statusCode).toBe(200);
-		expect(Array.isArray(response.body)).toBeTruthy();
-		expect(response.body.length).toBeGreaterThan(0);
-		expect(response.body[0].id).toBe(testUser.id);
-		console.log(response.body[0].user_achievements[0]);
-		expect(response.body[0].user_achievements[0].id).toBe(
-			testAchievement.id
-		);
-	});
-	afterAll(async () => {
-		try {
-			await User.destroy({ truncate: { cascade: true } });
-			await Achievement.destroy({ truncate: { cascade: true } });
-			await UserAchievements.destroy({ truncate: { cascade: true } });
-		} catch (error) {
-			console.error('Error cleaning up test data:', error);
-		}
-	});
-});
+  test('get user achievements by user id', async () => {
+    const response = await request(app).get(
+      `/api/user-achievements/${testUserId}`
+    )
+    console.log(response)
+    expect(response.statusCode).toBe(200)
+    expect(Array.isArray(response.body)).toBeTruthy()
+    expect(response.body.length).toBeGreaterThan(0)
+    expect(response.body[0].id).toBe(testUser.id)
+    console.log(response.body[0].user_achievements[0])
+    expect(response.body[0].user_achievements[0].id).toBe(testAchievement.id)
+  })
+  afterAll(async () => {
+    try {
+      await User.destroy({ truncate: { cascade: true } })
+      await Achievement.destroy({ truncate: { cascade: true } })
+      await UserAchievements.destroy({ truncate: { cascade: true } })
+    } catch (error) {
+      console.error('Error cleaning up test data:', error)
+    }
+  })
+})
