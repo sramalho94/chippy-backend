cb8da7dcde03563f1d376aa734b8ffcaf0208f5d
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-04-07 17:13:55 -0400

    Modified config file

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: 'chippy_development',
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_test,
-    host: process.env.DB_HOST_test,
-    dialect: 'postgres'
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  }
-}
+	development: {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: 'chippy_development',
+		host: process.env.DB_HOST,
+		dialect: 'postgres'
+	},
+	test: {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME_TEST,
+		host: process.env.DB_HOST_TEST,
+		dialect: 'postgres'
+	},
+	production: {
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: 'postgres'
+	}
+};
