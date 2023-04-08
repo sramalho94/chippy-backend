aee8a6b41f2002714919af2557d1f9f35c157836
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-07 21:16:05 -0400

    added conditional to check which .env to use

--- config/config.js
+++ config/config.js
@@ -1,25 +1,31 @@
-require('dotenv').config();
+require('dotenv').config()
+
+if (process.env.NODE_ENV === 'docker') {
+  require('dotenv').config({ path: '.env.docker' })
+} else {
+  require('dotenv').config({ path: '.env.local' })
+}
 
 module.exports = {
-	development: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: 'chippy_development',
-		host: process.env.DB_HOST,
-		dialect: 'postgres'
-	},
-	test: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME_TEST,
-		host: process.env.DB_HOST_TEST,
-		dialect: 'postgres'
-	},
-	production: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-		host: process.env.DB_HOST,
-		dialect: 'postgres'
-	}
-};
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'chippy_development',
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST || 'localhost',
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+}
